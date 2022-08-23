c31361d9b262f357ff463e3773540616276eaef6
Precious <Preshomondi@gmail.com>
2022-08-24 01:16:31 +0300

    commit tree 6ef67620b0645d317e31d577328d4362a6d8c6a5

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
   # write code here
+  def initialize(a,b,c)
+    @a=a
+    @b=b
+    @c=c
+
+  end
+
+  def kind
+   triangle = [@a, @b, @c].uniq
+   if(@a>0 && @b>0 && @c>0) && ((@a+@b)> @c && (@b+@c)>@a && (@a+@c) > @b)
+    if triangle.length == 1
+      :equilateral
+    elsif triangle.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+   else
+    begin
+      raise TriangleError
+    end
+   end
+  end
+  class TriangleError < StandardError
+  end
 end
